d2ef738a0f719d1e1fa663d9ba0e52f288d158d8
Piotr Sarna <sarna@chiselstrike.com>
2022-12-14 20:40:56 +0100

    Formatting

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,5 @@
 //use clap::{Parser, Subcommand};
+use anyhow::Result;
 
 struct Client {
     client: aws_sdk_s3::Client,
@@ -10,7 +11,7 @@ impl Client {
         Self { client, bucket }
     }
 
-    pub async fn list_generations(&self, db: &str) {
+    pub async fn list_generations(&self, db: &str) -> Result<()> {
         let mut next_marker = None;
         loop {
             let mut list_request = self
@@ -24,48 +25,39 @@ impl Client {
                 list_request = list_request.marker(marker)
             }
 
-            let response = list_request.send().await.unwrap();
+            let response = list_request.send().await?;
             let prefixes = match response.common_prefixes() {
                 Some(prefixes) => prefixes,
                 None => {
                     println!("No prefixes");
-                    return;
+                    return Ok(());
                 }
             };
 
-            println!(
-                "----------------------------------------------------------------------------"
-            );
-            println!(
-                "|                                Generations                               |"
-            );
-            println!(
-                "----------------------------------------------------------------------------"
-            );
-            println!(
-                "|                 uuid                 |             created at            |"
-            );
-            println!(
-                "----------------------------------------------------------------------------"
-            );
+            println!("----------------------------------------------------------------------");
+            println!("|                             Generations                            |");
+            println!("----------------------------------------------------------------------");
+            println!("|                uuid                  |         created at          |");
+            println!("----------------------------------------------------------------------");
             for prefix in prefixes {
                 if let Some(prefix) = &prefix.prefix {
                     let prefix = &prefix[db.len() + 1..prefix.len() - 1];
-                    let uuid = uuid::Uuid::try_parse(prefix).unwrap();
-                    let (seconds, nanos) = uuid.get_timestamp().unwrap().to_unix();
-                    let (seconds, nanos) = (253370761200 - seconds, 999999999 - nanos);
-                    let date =
-                        chrono::NaiveDateTime::from_timestamp_opt(seconds as i64, nanos).unwrap();
-                    println!("| {} | {:>30} UTC |", uuid, date);
+                    let uuid = uuid::Uuid::try_parse(prefix)?;
+                    let (seconds, nanos) = uuid
+                        .get_timestamp()
+                        .map(|ts| ts.to_unix())
+                        .unwrap_or((0, 0));
+                    let (seconds, nanos) = (253370761200 - seconds, 999000000 - nanos);
+                    let date = chrono::NaiveDateTime::from_timestamp_opt(seconds as i64, nanos)
+                        .unwrap_or(chrono::NaiveDateTime::MIN);
+                    println!("| {} | {:>24} UTC |", uuid, date);
                 }
             }
-            println!(
-                "----------------------------------------------------------------------------"
-            );
+            println!("----------------------------------------------------------------------");
 
             next_marker = response.next_marker().map(|s| s.to_owned());
             if next_marker.is_none() {
-                break;
+                return Ok(());
             }
         }
     }
@@ -82,5 +74,5 @@ async fn main() {
         std::env::var("LIBSQL_BOTTOMLESS_BUCKET").unwrap_or_else(|_| "bottomless".to_string());
     let client = Client::new(aws_sdk_s3::Client::new(&loader.load().await), bucket);
 
-    client.list_generations("test.db").await;
+    client.list_generations("test.db").await.unwrap();
 }
