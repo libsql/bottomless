6ad7de27f6032c42f8931a18a53ff4c4f5fce2f8
Piotr Sarna <sarna@chiselstrike.com>
2022-12-19 14:35:08 +0100

    test: make db immutable in restore_test

--- test/restore_test.sql
+++ test/restore_test.sql
@@ -1,7 +1,7 @@
 .bail on
 .echo on
 .load ../target/debug/bottomless
-.open file:test.db?wal=bottomless
+.open file:test.db?wal=bottomless&immutable=1
 PRAGMA journal_mode=wal;
 .mode column
 SELECT v, length(v) FROM test;
